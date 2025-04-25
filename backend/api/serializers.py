fb80073187d1deb771f67fc702c6f773fc1021d2
ribeirin-7 <arthurr366@gmail.com>
2025-04-25 15:43:41 -0300

    Implementação dos serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,61 @@
 from django.db.models import fields
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from .models import *
 
+class UserSerializer(serializers.ModelSerializer):
+    nome = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'nome', 'isAdmin']
+
+    def get_id(self, obj):
+        return obj.id
+    
+    def get_isAdmin(self, obj):
+        #verifica se é admin ou usuário normal
+        return obj.is_staff
+    
+    def get_nome(self, obj):
+        #se o nome estiver em branco, atribui o email ao nome
+        nome = obj.first_name
+        if nome == "":
+            nome = obj.email
+        return nome
+    
+class UserSerializerComToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'nome', 'isAdmin', 'token']
+    
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
+    
+class RevisaoSerializer(serializers.Serializer):
+    class Meta:
+        model = Revisao
+        fields = '__all__'
+
+class ProdutoSerializer(serializers.Serializer):
+    revisoes = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = Produto
+        fields = '__all__'
+
+    def get_revisoes(self, obj):
+        revisoes = obj.revisao_set.all()
+        serializer = RevisaoSerializer(revisoes, many=True)
+        return serializer.data
+    
+class EnderecoDeEntregaSerializer(serializers.Serializer):
+    class Meta:
+        model = EnderecoDeEntrega
+        fields = '__all__'
