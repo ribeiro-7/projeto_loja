be9ecdf09a7c99c15507084506236ddd4a38ea0d
ribeirin-7 <arthurr366@gmail.com>
2025-04-25 11:39:16 -0300

    Criação do modelo pedido

--- backend/api/models.py
+++ backend/api/models.py
@@ -19,3 +19,30 @@ class Produto(models.Model):
     def __str__(self):
         return self.nome +  " | " + self.marca + " | " + str(self.preco)
     
+class Revisao(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    nome = models.CharField(max_length=200, null=True, blank=True)
+    avaliacao = models.IntegerField(null=True, blank=True, default=0)
+    comentario = models.TextField(null=True, blank=True)
+    criadoEm = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.avaliacao)
+
+class Pedido(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    metodoDePagamento = models.CharField(max_length=200, null=True, blank=True)
+    precoImposto = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
+    precoFrete = models.DecimalField(mmax_digits=12, decimal_places=2, null=True, blank=True)
+    precoFinal = models.DecimalField(mmax_digits=12, decimal_places=2, null=True, blank=True)
+    pagamentoConcluido = models.BooleanField(default=False)
+    pagoEm = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    envioFeito = models.BooleanField(default=False)
+    enviadoEm = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    criadoEm = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.criadoEm)
